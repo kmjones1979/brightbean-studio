36c9790d304a16c1a880d0733c56beed198ab6f2
Jan Schmitz <jan.schmitz@whu.edu>
2026-04-17 11:40:52 +0200

    added blob to CSP_MEDIA_SRC

--- config/settings/base.py
+++ config/settings/base.py
@@ -254,7 +254,7 @@ CSP_STYLE_SRC = ("'self'", "'unsafe-inline'", "https://cdn.jsdelivr.net")
 CSP_IMG_SRC = ("'self'", "data:", "https:")
 CSP_FONT_SRC = ("'self'",)
 CSP_CONNECT_SRC = ("'self'",)
-CSP_MEDIA_SRC = ("'self'",)
+CSP_MEDIA_SRC = ("'self'", "blob:")
 CSP_FORM_ACTION = ("'self'", "https://accounts.google.com")
 CSP_INCLUDE_NONCE_IN = ["script-src"]
 
